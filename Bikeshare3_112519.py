fe61d3940dd1182a7e0dfb2d04736b0494bf9889
Chris Lautemann <lauteman@gmail.com>
2019-12-16 13:20:21 +0100

    Add y as possible input

--- Bikeshare3_112519.py
+++ Bikeshare3_112519.py
@@ -180,7 +180,7 @@ def main():
         display_lines(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
+        if (restart.lower() != 'yes' or restart.lower() != 'y'):
             break
 
 
